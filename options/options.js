a261298a579ecd027a98f3cfbca39561737dd6f0
Crowdbotics <team@crowdbotics.com>
2024-02-05 19:26:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blockchainidentityv-45974",
+  url: "https://blockchainidentityv-45974.botics.co",
+  api: "https://blockchainidentityv-45974.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
